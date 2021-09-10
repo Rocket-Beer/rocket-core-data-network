a3884d8fb89bac23521f20b05743842f9666d454
Rui Miguel Alonso <ruiskas@gmail.com>
2021-09-10 10:07:28 +0200

    Build: upgrade network dependencies

--- core-data-network/build.gradle.kts
+++ core-data-network/build.gradle.kts
@@ -46,15 +46,15 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.30")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.annotation:annotation:1.2.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-native-mt")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
     implementation("com.rocket.core:core-domain:0.0.3-alpha6")
     implementation("com.rocket.core:crash-reporting:0.0.3-alpha6")
     implementation("com.rocket.android.core:crash-reporting-android:0.0.3-alpha7")
-    implementation("com.rocket.core:core-data-network-commons:0.0.3-alpha4")
+    implementation("com.rocket.core:core-data-network-commons:0.0.3-alpha5")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
@@ -62,14 +62,14 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.10.0")
 
-    testImplementation("com.rocket.android.core:core-data-network-test:0.0.2-alpha4")
+    testImplementation("com.rocket.android.core:core-data-network-test:0.0.2-alpha5")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.20")
     testImplementation("io.mockk:mockk:1.12.0")
     testImplementation("io.mockk:mockk-android:1.12.0")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.9.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0")
     testImplementation("androidx.test.ext:junit:1.1.3")
 }
