a42a1851adcff1d263d31f186f0e837c9055fb88
droldanb@emeal.nttdata.com <cF0T0O6K4l>
2022-09-16 14:59:34 +0200

    [FEATURE] Apply logPath parameter to TestLogger class.

--- core-data-network-test/src/main/java/com/rocket/android/core/data/network/test/logger/TestLogger.kt
+++ core-data-network-test/src/main/java/com/rocket/android/core/data/network/test/logger/TestLogger.kt
@@ -2,15 +2,26 @@ package com.rocket.android.core.data.network.test.logger
 
 import com.rocket.core.crashreporting.logger.CrashLogger
 import com.rocket.core.crashreporting.logger.LogLevel
+import java.io.File
 
 class TestLogger : CrashLogger {
-    override fun log(message: String, map: Map<String, String?>, logLevel: LogLevel) {
+    override fun log(
+        message: String,
+        map: Map<String, String?>,
+        logLevel: LogLevel,
+        logPath: File?
+    ) {
         println(
             "[${logLevel.name.first().titlecase()}] TestLogger: $message - $map"
         )
     }
 
-    override fun log(exception: Throwable, map: Map<String, String?>, logLevel: LogLevel) {
+    override fun log(
+        exception: Throwable,
+        map: Map<String, String?>,
+        logLevel: LogLevel,
+        logPath: File?
+    ) {
         println(
             "[${logLevel.name.first().titlecase()}] TestLogger: ${exception.message} - $map"
         )
